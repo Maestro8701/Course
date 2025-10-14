254f79d68149d7ec02429e66ce962cbb4b7b5b09
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2025-10-15 04:03:29 +0700

    Create main.cpp

---
+++ QTCalculator/main.cpp
@@ -0,0 +1,10 @@
+// В этом файле только функция main и #include-директивы.
+#include <iostream>
+#include <string>
+#include  <cmath>
+
+#include "calculator.h"
+
+int main() {
+    RunCalculatorCycle();
+}
