3e64508069f65f09b7eea0eabde14ec65f57bc49
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2025-11-01 04:11:01 +0700

    Update calculator.h

--- QTCalculator/calculator.h
+++ QTCalculator/calculator.h
@@ -1,7 +1,39 @@
 #pragma once
 
+#include <string>
+
 using Number = double;
 
-bool ReadNumber(Number& operand);
+class Calculator {
+public:
 
-bool RunCalculatorCycle();
+    void Set(Number n);
+
+    Number GetNumber() const;
+
+    void Add(Number n);
+
+    void Sub(Number n);
+
+    void Div(Number n);
+
+    void Mul(Number n);
+
+    void Pow(Number n);
+
+    void Save();
+
+    void Load();
+
+    bool HasMem() const;
+
+    std::string GetNumberRepr() const;
+
+private:
+    Number current_value_;
+    Number memory_slot_;
+    bool memory_state_ = false;
+
+};
+
+
