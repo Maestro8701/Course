6d24a299dddbe0e0184808db78b64cdf029fb9c9
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2026-05-15 00:49:36 +0700

    Update calculator.cpp

--- QTCalculator/calculator.cpp
+++ QTCalculator/calculator.cpp
@@ -29,21 +29,6 @@ void Calculator::Pow(Number n) {
     current_value_ = std::pow(current_value_, n);
 }
 
-/*
-void Calculator::Save() {
-    memory_state_ = true;
-    memory_slot_ = current_value_;
-}
-
-void Calculator::Load() {
-    current_value_ = memory_slot_;
-}
-
-bool Calculator::HasMem() const {
-    return memory_state_;
-}
-*/
-
 std::string Calculator::GetNumberRepr() const {
     return std::to_string(current_value_);
 }
